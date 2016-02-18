0627a439409490c2cdee5e787ce92c383aac0add
ovidiu <github@ovidiu.me>
2016-02-18 11:29:23 +0200

    spell check and minor optimization

--- octoprint_title_status/__init__.py
+++ octoprint_title_status/__init__.py
@@ -12,33 +12,34 @@ class TitleStatusPlugin(octoprint.plugin.AssetPlugin, octoprint.plugin.EventHand
 			)
 			
 	def get_state_id(self):
-		state = self._printer._state
-		if state == None or state == 0:
-			return "OFFLINE"
-		if state == 4:
-			return "CONNECTING"
-		if state == 5:
-			return "OPERATIONAL"
-		if state == 6:
-			return "PRINTING"
-		if state == 7:
-			return "PAUSED"
-		if state == 8:
-			return "CLOSED"
-		if state == 9:
-			return "ERROR"
-		if state == 10:
-			return "CLOSED_WITH_ERROR"
-		if state in [1, 2, 3, 11]:
-			return "OTHER"
-		return "ÜNKNOWN"
+		try: 
+			return self._printer.get_state_id()
+		except:
+			state = self._printer._state
+			#see /octoprint/util/comm.py for state values
+			if state == None or state == 0:
+				return "OFFLINE"
+			if state == 4:
+				return "CONNECTING"
+			if state == 5:
+				return "OPERATIONAL"
+			if state == 6:
+				return "PRINTING"
+			if state == 7:
+				return "PAUSED"
+			if state == 8:
+				return "CLOSED"
+			if state == 9:
+				return "ERROR"
+			if state == 10:
+				return "CLOSED_WITH_ERROR"
+			if state in [1, 2, 3, 11]:
+				return "OTHER"
+			return "UNKNOWN"
 		
 	def on_event(self, event, payload):
 		if event == Events.CLIENT_OPENED:
-			try: 
-				self._plugin_manager.send_plugin_message(self._identifier, self._printer.get_state_id())
-			except:
-				self._plugin_manager.send_plugin_message(self._identifier, self.get_state_id())
+			self._plugin_manager.send_plugin_message(self._identifier, self.get_state_id())
 
 	def get_version(self):
 		return self._plugin_version
